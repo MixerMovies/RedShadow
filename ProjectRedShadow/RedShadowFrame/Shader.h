9d5948acc469bff33e481a7d94f23c99b4eb5956
aareschluchtje <remcosannen@gmail.com>
2017-03-09 21:52:52 +0100

    Added more methods to the shader class

--- ProjectRedShadow/RedShadowFrame/Shader.h
+++ ProjectRedShadow/RedShadowFrame/Shader.h
@@ -1,8 +1,15 @@
 #pragma once
 
-#include <gl\glew.h>
+#include <GL\glew.h>
 
 class Shader
 {
-	
+	Shader(std::string vsfile, std::string fsfile);
+	void checkShaderErrors(GLuint shaderId);
+	void link(void);
+	void use(void);
+	void setUniform(GLuint, const GLchar*);
+	virtual void bindAttributes() = 0;
+	GLuint programId;
+	GLuint Uniform;
 };
