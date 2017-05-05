e9e12f61a6910574a64462122fbca0187da23742
Aareschluchtje <remcosannen@gmail.com>
2017-05-05 22:07:01 +0200

    Finished screenwarp shader

--- ProjectRedShadow/ProjectRedShadow/Shaders/screenwarp.fs
+++ ProjectRedShadow/ProjectRedShadow/Shaders/screenwarp.fs
@@ -5,13 +5,15 @@ varying vec2 texCoord;
 
 void main()
 {
+	float length = sqrt(((texCoord.x - 0.5) * (texCoord.x - 0.5)) + ((texCoord.y - 0.5) * (texCoord.y - 0.5)));
+
 	mat3 rotationMatrix = mat3(
-		cos(time), sin(time), 0,
-		-sin(time), cos(time), 0,
+		cos(time * length), -sin(time * length), 0,
+		sin(time * length), cos(time * length), 0,
 		0, 0, 1.0
 	);
 
-	vec3 rotatedPosition = vec3(texCoord.x, texCoord.y, 0);
+	vec3 rotatedPosition = vec3(texCoord.x - 0.5, texCoord.y - 0.5, 0);
 	rotatedPosition = rotationMatrix * rotatedPosition;
-	gl_FragColor = texture2D(s_texture, vec2(rotatedPosition.x, rotatedPosition.y));
+	gl_FragColor = texture2D(s_texture, vec2(rotatedPosition.x + 0.5, rotatedPosition.y + 0.5));
 }
