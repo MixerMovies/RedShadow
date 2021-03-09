85fe10c7ec135e529e934b79e787818d50776858
Remco <remcosannen@gmail.com>
2021-03-09 22:54:51 +0100

    continued implementation

--- ProjectRedShadow/ProjectRedShadow/Floor.cpp
+++ ProjectRedShadow/ProjectRedShadow/Floor.cpp
@@ -21,5 +21,17 @@ void Floor::GenerateFloor()
 		vertices.push_back(glm::sin(M_PI_2 * i + M_PI_4) * _size);
 	}
 
-	//_objModel = new ObjModel(vertices, , , , new Texture(FileLoader::getMainPath() + "\\Textures\\White.png") );
+	std::vector<float> normals = std::vector<float>();
+	normals.push_back(0);
+	normals.push_back(1);
+	normals.push_back(0);
+
+	std::vector<float> textureCoordinates = std::vector<float>();
+	for (int i = 0; i < 4; i++)
+	{
+		textureCoordinates.push_back(glm::cos(M_PI_2 * i + M_PI_4));
+		textureCoordinates.push_back(glm::sin(M_PI_2 * i + M_PI_4));
+	}
+
+	_objModel = new ObjModel(vertices, normals, textureCoordinates, , new Texture(FileLoader::getMainPath() + "\\Textures\\White.png") );
 }
