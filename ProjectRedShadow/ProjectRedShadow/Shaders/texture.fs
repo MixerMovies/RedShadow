543c3923f3ffb5f6f28164fe77aadc349960c3fb
Remco <remcosannen@gmail.com>
2021-04-20 21:27:35 +0200

    added ambient to the startup shader

--- ProjectRedShadow/ProjectRedShadow/Shaders/texture.fs
+++ ProjectRedShadow/ProjectRedShadow/Shaders/texture.fs
@@ -2,9 +2,10 @@
 
 uniform sampler2D s_texture;
 uniform vec3 diffuse;
+uniform vec3 ambient;
 in vec2 texCoord;
 
 void main()
 {
-    gl_FragColor = texture2D(s_texture, texCoord) * vec4(diffuse, 1);
+    gl_FragColor = texture2D(s_texture, texCoord) * (vec4(diffuse, 1) + vec4(ambient, 1));
 }
